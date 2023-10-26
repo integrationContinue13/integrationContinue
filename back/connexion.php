a945faa97a4ab78a55e937e61fd9b720cec0066b
Julien <julien.paiva10@gmail.com>
2023-10-26 10:45:17 +0200

    cleanup directory

--- back/connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Informations de connexion à la base de données
-$host = '192.168.81.239'; // Adresse du serveur MySQL
-$dbname = 'devworkshop'; // Nom de la base de données
-$username = 'devworkshop'; // Nom d'utilisateur MySQL
-$password = '2R0tOz253Zat'; // Mot de passe MySQL
-?>
