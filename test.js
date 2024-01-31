51f9636a2960320a65b96307a69422721017dbd1
NoLuck4git <148863483+NoLuck4git@users.noreply.github.com>
2024-01-31 17:32:01 +0100

    Update test.js

--- test.js
+++ test.js
@@ -9,19 +9,14 @@ global.localStorage = {
 
 describe('Vérification du panier', function() {
     it('Devrait afficher un message si le panier contient des éléments', function() {
-        // Attendez un peu pour laisser le temps au panier d'être potentiellement mis à jour
         setTimeout(function() {
             // Récupérez le contenu du panier depuis le localStorage
             var panierActuel = localStorage.getItem("panier");
-
-            // Vérifiez si le panier contient quelque chose
             if (panierActuel && JSON.parse(panierActuel).length > 0) {
-                // Utilisez assert pour vérifier que le panier contient des éléments
                 assert.ok(true, 'Le panier contient des éléments');
             } else {
-                // Utilisez assert pour vérifier que le panier est vide
                 assert.ok(true, 'Le panier est vide');
             }
-        }, 500); // Attendez 500 millisecondes (ajustez selon vos besoins)
+        }, 500); 
     });
 });
